c78f8d1c23b76a3bd3d21ec34857215ab5c68715
mo5e5 <malteoppermann@gmx.net>
2023-10-31 11:20:05 +0100

    ...

--- src/golden syntax/DamageHero.kt
+++ src/golden syntax/DamageHero.kt
@@ -1,17 +1,33 @@
 class DamageHero(name: String, healthbar: Int) : Hero(name, healthbar) {
 
+    /**
+     *  Schleuder das Schwert in richtung des AntiHelden dieser wird mehrfach von der rotierenden
+     *  Klinge getroffen und erleidet 15 % seiner gesamten Lebensenergie an Schaden.
+     */
     fun swordSpin() {
 
     }
 
+    /**
+     *  Schlägt mit dem Griff seines Schwertes fest zu und fügt dem AntiHelden 10 % seiner gesamten Lebensenergie
+     *  an Schaden zu.
+     */
     fun knopStrike() {
 
     }
 
+    /**
+     *  Hält das Schwert schützend vor sich und blockt so 50 % vom nächsten Schaden der auf ihm gewirkt wird.
+     */
     fun swordBlock() {
 
     }
 
+    /**
+     *  Blitze kommen aus dem Himmel geflogen. Diese treffen einen AntiHeld mit einem Schaden von 30 %
+     *  der gesamten Lebensenergie des AntiHelden. Diese Fähigkeit geht nach benutzen für 5 Runden in den
+     *  Cooldown.
+     */
     fun thunderSword() {
 
     }
