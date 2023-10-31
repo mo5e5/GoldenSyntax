fe75bd87a8f962e3c360899e717095517581ffff
mo5e5 <malteoppermann@gmx.net>
2023-10-31 11:10:14 +0100

    ...

--- src/golden syntax/TankHero.kt
+++ src/golden syntax/TankHero.kt
@@ -2,18 +2,30 @@ import Hero
 
 class TankHero(name: String, healthbar: Int) : Hero(name, healthbar) {
 
+    /**
+     *  Heilt diesen Helden um 10 % seiner gesamten Lebensenergie.
+     */
     fun heal() {
 
     }
 
+    /**
+     *  Zieht die Aufmerksamkeit aller AntiHelden für zwei Runden auf sich.
+     */
     fun drawAttention() {
 
     }
 
+    /**
+     *  Ein heftiger Schlag der den getroffenen AntiHeld 15 % seiner gesamten Lebensenergie abzieht.
+     */
     fun punch() {
 
     }
 
+    /**
+     *  Ein Roundhouse Kick aller Chuck Norris der den AntiHeld 25 % seiner gesamten Lebensenergie abzieht.
+     */
     fun kick() {
 
     }
