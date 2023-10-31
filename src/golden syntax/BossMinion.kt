71c05717295a8c8ffb58e02cd23ffba39d52f5ba
mo5e5 <malteoppermann@gmx.net>
2023-10-31 10:53:25 +0100

    ...

--- src/golden syntax/BossMinion.kt
+++ src/golden syntax/BossMinion.kt
@@ -1,5 +1,8 @@
 class BossMinion(name: String, healthbar: Int) : AntiHero(name, healthbar) {
 
+    /**
+     *  Zieht die Aufmerksamkeit aller Helden für eine Runde auf den BossMinion.
+     */
     fun drawAttention() {
         println(
             "${UNDERTAKER.name} zieht die Aufmerksamkeit aller Helden auf sich.\n" +
@@ -8,14 +11,25 @@ class BossMinion(name: String, healthbar: Int) : AntiHero(name, healthbar) {
         )
     }
 
+    /**
+     *  Ein Schild der 50 % der gesamten Lebensenergie beträgt. Er hält bis er zerstört wird.
+     *  Hat nach seiner zerstörung drei Runden Cooldown.
+     */
     fun shield() {
-
     }
 
+    /**
+     *  Ein kraftvoller Schlag der einem Helden 5 % seiner gesamten Lebensenergie abzieht.
+     */
     fun punsh() {
 
     }
 
+    /**
+     *  Eine Special Attacke die erst nach 5 Runden zur verfügung steht
+     *  und nach benutzen neu aufgeladen werden muss.
+     *  Sie fügt allen Helden 10 % ihrer gesamten Lebensenergie an Schaden zu.
+     */
     fun hailBlow() {
 
     }
