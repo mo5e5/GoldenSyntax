e4935e4bf5a334326234809a9fd9020a3df58104
mo5e5 <malteoppermann@gmx.net>
2023-10-31 11:28:28 +0100

    ...

--- src/golden syntax/MagicHero.kt
+++ src/golden syntax/MagicHero.kt
@@ -2,18 +2,34 @@ import Hero
 
 class MagicHero(name: String, healthbar: Int) : Hero(name, healthbar) {
 
+    /**
+     *  Es hagelt Blitze vom Himmel die alle AntiHelden treffen und jeweils 5 % der gesamten Lebensenergie
+     *  an Schaden verursachen.
+     */
     fun lightningStrike() {
 
     }
 
+    /**
+     *  Die Erde erhebt sich und fügt dem AntiHeld der auf ihr steht, 10 % der gesamten Lebensenergie an schaden zu.
+     */
     fun earthquake() {
 
     }
 
+    /**
+     *  Ein Schutzzauber der auf jeden Helden wirkt und 15 % des eingehen Schadens blockiert.
+     *  Der Zauber hält 3 Runden.
+     */
     fun blockDamageBuff() {
 
     }
 
+    /**
+     *  Macht den Helden für 2 Runden unsterblich wobei der eingehende Schaden trotzdem von seinen
+     *  Lebenspunkten abgezogen wird.
+     *  Hat nach Benutzung 3 Runden Cooldown.
+     */
     fun immortality() {
 
     }
