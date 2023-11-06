5d640a8e1c1c4b40d7f687e873cc3774800296ca
mo5e5 <malteoppermann@gmx.net>
2023-11-06 09:40:31 +0100

    ...

--- src/golden syntax/MagicBag.kt
+++ src/golden syntax/MagicBag.kt
@@ -3,29 +3,22 @@ open class MagicBag {
     /**
      *  Ein mächtiger Trank der seinem Benutzer 20 % seiner gesamten Lebensenergie zurückgibt.
      */
-    fun heallItem() {
+    fun heallItem(hero: Hero) {
 
     }
 
     /**
-     *  Ein mächtiges Schutzpulver was über einer Gruppe ein Schild bildet und keinen Schaden mehr durchlässt.
+     *  Ein mächtiges Schutzpulver was jedem Gruppen Mitglied 10 % seiner gesamten Lebensenergie zurückgibt.
      *  Leider hält es nur eine Runde.
      */
-    fun buffItemTeam() {
+    fun buffItemTeam(list: List<Hero>) {
 
     }
 
     /**
-     *  Ein mächtiger Trank der seinem Benutzer 20 % mehr Schaden auf seinen nächsten Angriff gibt.
+     *  Ein mächtiger Trank der seinem Benutzer dauerhaft 20 % mehr Schaden gibt.
      */
-    fun buffItem() {
-
-    }
-
-    /**
-     *  Entfernt einen debuff vom Benutzer.
-     */
-    fun debuffItem() {
+    fun buffItem(hero: Hero) {
 
     }
 
