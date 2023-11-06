137f6e273b600dfca7a2736354fdf0ac7ea0be53
mo5e5 <malteoppermann@gmx.net>
2023-11-06 10:12:03 +0100

    ...

--- src/golden syntax/Hero.kt
+++ src/golden syntax/Hero.kt
@@ -1,7 +1,6 @@
 open class Hero(name: String, healthbar: Int) : Character(name, healthbar) {
 
     fun useMagicBag() {
-
         println(
             "$name greift in den Magischen Beutel.\n" +
                     "Nach kurzer Zeit des wühlens findet er ..."
