7e1483bdd64453d88106901b788e504c8c782443
mo5e5 <malteoppermann@gmx.net>
2023-11-07 19:41:47 +0100

    ...

--- src/golden syntax/MagicBag.kt
+++ src/golden syntax/MagicBag.kt
@@ -1,26 +1,26 @@
 open class MagicBag {
 
     /**
-     *  Ein mächtiger Trank der seinem Benutzer 20 % seiner gesamten Lebenspunkte zurückgibt.
+     *  Ein mächtiger Trank der seinem Benutzer 20 % seiner gesamten Lebensenergie zurückgibt.
      */
     fun heallItem(hero: Hero) {
         val useHealItem = hero.maxHealth + hero.maxHealth * 0.2
         hero.maxHealth = useHealItem.toInt()
-        println("${hero.name} hat nun ${hero.healthbar} Lebenspunkte.")
+        println("${hero.name} heilt sich um ${hero.healthbar} Lebensenergie.")
     }
 
     /**
-     *  Ein mächtiges Schutzpulver was jedem Gruppen Mitglied 10 % seiner gesamten Lebenspunkte zurückgibt.
+     *  Ein mächtiges Schutzpulver was jedem Gruppen Mitglied 10 % seiner gesamten Lebensenergie zurückgibt.
      *  Leider hält es nur eine Runde.
      *
      *  @param list bekommt eine Liste von Helden übergeben.
      */
     fun healItemTeam(list: List<Hero>) {
-        println("Alle Helden erhalten 10 % ihrer Lebenspunkte zurück.")
+        println("Alle Helden erhalten eine 10 % Heilung.")
         for (hero in list) {
             val useHealItemTeam = hero.healthbar + hero.maxHealth * 0.1
             hero.healthbar = useHealItemTeam.toInt()
-            println("${hero.name} hat nun ${hero.healthbar} Lebenspunkte.")
+            println("${hero.name} heilt sich um ${hero.healthbar} Lebensenergie.")
         }
     }
 
@@ -33,5 +33,4 @@ open class MagicBag {
         hero.damageMultiplier += 0.2
         println("${hero.name} macht nun 20 % mehr Schaden.")
     }
-
 }
