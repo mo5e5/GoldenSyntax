d2ca2ed7ceef3af9ad15afe557ba40b9cd05d456
mo5e5 <malteoppermann@gmx.net>
2023-11-08 23:08:12 +0100

    17 Uhr Push

--- src/golden syntax/test.kt
+++ src/golden syntax/test.kt
@@ -1,23 +1,25 @@
 fun main() {
 
+    /*
     val person = "Malte"
     println("${RED}${person}${RESET}")
     println("${GREEN}${person}${RESET}")
-    println("${WHIGHT}${person}${RESET}")
+    println("${WHITE}${person}${RESET}")
     println("${BLACK}${person}${RESET}")
     println("${BOLD}${person}${RESET}")
     println("${BACKGROUNBLACK}${person}${RESET}")
-    println("${BACKGROUNDWHIGHT}${RED}${person}${RESET}")
+    println("${BACKGROUNDWHITE}${RED}${person}${RESET}")
     println()
     println("${BACKGROUNBLACK}${GREEN}${BOLD}Das Gute hat gesiegt.${RESET}")
 
-    println("${BACKGROUNDWHIGHT}${BLACK}Das helle Seite der Macht ist am Zug.${RESET}")
+    println("${BACKGROUNDWHITE}${BLACK}Das helle Seite der Macht ist am Zug.${RESET}")
 
     println("${BACKGROUNBLACK}${RED}${BOLD}Das Böse war Siegreich.${RESET}")
 
-    println("${BACKGROUNBLACK}${WHIGHT}Die dunkle Seite der Macht ist am Zug.${RESET}")
+    println("${BACKGROUNBLACK}${WHITE}Die dunkle Seite der Macht ist am Zug.${RESET}")
 
     println("${BOLD}-------${RESET}")
     println("${BLACK}${BOLD}-------${RESET}")
+    */
 
 }
